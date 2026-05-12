56b68adfdac0ffd569e1a7e11437284e6cc30b1d
Monika <monika.dembek@gmail.com>
2026-05-12 15:11:28 +0200

    Add home component

--- apps/opticv-web/src/app/app.routes.ts
+++ apps/opticv-web/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home').then((c) => (c.Home))
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
---
+++ apps/opticv-web/src/app/features/home/home.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+
+@Component({
+  selector: 'app-home',
+  imports: [ButtonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Home {
+  private router = inject(Router);
+
+  goToCreator(): void {
+    this.router.navigate(['optimize-cv']);
+  }
+
+  signUp(): void {
+    // go to sign up
+  }
+}
