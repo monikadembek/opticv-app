df78fce3b6414b886558bb148bb7604b4a26c4f9
Monika <monika.dembek@gmail.com>
2026-05-28 15:12:26 +0200

    Task 37: Add staging environment to frontend app

---
+++ apps/opticv-web/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://kmkkoqsagagernsxuggr.supabase.co',
+  supabaseKey: 'sb_publishable_LkGYwEGlTn2nNLDa0np0kw_GXlZKcvl',
+  apiUrl: 'https://api.opticv.com/api',
+};
---
+++ apps/opticv-web/src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://kmkkoqsagagernsxuggr.supabase.co',
+  supabaseKey: 'sb_publishable_LkGYwEGlTn2nNLDa0np0kw_GXlZKcvl',
+  apiUrl: 'https://opticv-app.onrender.com/api',
+};
