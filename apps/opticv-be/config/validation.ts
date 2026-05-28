1df7018a3f1bd5d4d9f2908c2ea567e8ed9bde1b
Monika <monika.dembek@gmail.com>
2026-05-28 16:26:16 +0200

    Task 37: Update validation in config module adding staging node env

--- apps/opticv-be/config/validation.ts
+++ apps/opticv-be/config/validation.ts
@@ -1,7 +1,7 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  NODE_ENV: Joi.string().valid('development', 'production'),
+  NODE_ENV: Joi.string().valid('development', 'staging', 'production'),
   PORT: Joi.number().default(3000),
   SUPABASE_URL: Joi.string().uri().required(),
   SUPABASE_PUBLISHABLE_KEY: Joi.string().required(),
