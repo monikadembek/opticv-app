e0672c04b6235b1ebc83566a433443d2501a28b4
Monika <monika.dembek@gmail.com>
2026-07-03 09:54:19 +0200

    Task 60: Add Sentry to nestjs app

--- apps/opticv-be/src/main.ts
+++ apps/opticv-be/src/main.ts
@@ -2,6 +2,7 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
+import './instrument';
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
