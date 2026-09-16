0bf2395189d0c1db8e1bceffb56bf981f90ef554
Monika <monika.dembek@gmail.com>
2026-09-16 14:27:49 +0200

    Task 118: Update frontend prod env

--- apps/opticv-web/src/environments/environment.prod.ts
+++ apps/opticv-web/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  supabaseUrl: 'https://kmkkoqsagagernsxuggr.supabase.co',
-  supabaseKey: 'sb_publishable_LkGYwEGlTn2nNLDa0np0kw_GXlZKcvl',
-  apiUrl: 'https://api.opticv.com/api',
+  supabaseUrl: 'https://bvpnkawgdaqfuefripjy.supabase.co',
+  supabaseKey: 'sb_publishable_CTQBk1PaWAdylR4wogaL_A_ncqVvzn8',
+  apiUrl: 'https://api.opticv.net/api',
   posthogKey: 'phc_zTVrDYQVBnNi3yRaXduMshuKeNPoPd5u7MXzC9Gwb7Qa',
   posthogHost: 'https://eu.i.posthog.com',
 };
